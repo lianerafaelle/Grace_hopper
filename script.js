b79f79383b24ab5c16cd4b3b4047636db7f3803e
Liane Rafaelle <141535641+lianerafaelle@users.noreply.github.com>
2023-10-11 14:31:52 -0300

    script.js
    No JavaScript, um carrossel de imagens foi implementado para o site sobre Grace Hopper. Uma matriz images contém os caminhos das imagens correspondentes à Grace Hopper em diferentes momentos de sua vida, e uma matriz descriptions contém descrições correspondentes para cada imagem.
    
    O código manipula o DOM para exibir as imagens em um elemento de classe "gallery". Ao carregar a página, a primeira imagem é exibida. Dois botões de navegação, "Anterior" e "Próxima", foram implementados para permitir que os usuários naveguem pelas imagens. Quando o botão "Próxima" é clicado, a próxima imagem na matriz é exibida. Se o botão "Anterior" é clicado, a imagem anterior é exibida. O carrossel é cíclico, ou seja, depois da última imagem, a primeira imagem é exibida ao clicar em "Próxima", e vice-versa.
    
    O código também garante que as descrições correspondentes às imagens sejam exibidas, garantindo uma experiência informativa para os usuários. Os eventos de clique nos botões de navegação são monitorados e manipulados para garantir a funcionalidade do carrossel de imagens no site.

---
+++ script.js
@@ -0,0 +1,45 @@
+const images = [
+    'img/graceanalista.jpg',
+    'img/gracedandoaula.jpg',
+    'img/graceemmaquinas.jpeg',
+    'img/gracejovem.jpg', 
+    'img/gracerecebendomedalhas.jpg', 
+    'img/graceatual.jpg',
+];
+
+const descriptions = [
+    'Grace Hopper como analista de sistemas.',
+    'Grace Hopper dando aula em um evento.',
+    'Grace Hopper trabalhando em computadores.',
+    'Foto de Grace Hopper quando jovem.',
+    'Grace Hopper recebendo medalhas e prêmios.',
+    'Foto atual de Grace Hopper.',
+];
+
+const galleryContainer = document.querySelector('.gallery');
+let currentIndex = 0;
+
+function showImage(index) {
+    galleryContainer.innerHTML = ''; // Limpa o conteúdo anterior
+    const imgElement = document.createElement('img');
+    imgElement.src = images[index];
+    imgElement.alt = descriptions[index];
+    galleryContainer.appendChild(imgElement);
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
+
+showImage(currentIndex); // Exibe a primeira imagem inicialmente
+
+// Define eventos para os botões de próximo e anterior
+document.querySelector('.next-button').addEventListener('click', nextImage);
+document.querySelector('.prev-button').addEventListener('click', prevImage);
+
